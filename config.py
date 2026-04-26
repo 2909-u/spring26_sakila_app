c094ab1c40227ef3473ff12b3b97b1d3f98e2c00
hadijalal678 <143265968+hadijalal678@users.noreply.github.com>
2026-04-26 15:59:25 +0500

    fix(config): restore Config class to fix import error

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 # ============================================
-# Author: Abdul Hadi Jalal (F2023-523)
+# Author: Abdul Hadi Jalal
 # Date: 2026-04-25
 # Purpose: Database configuration for Sakila Flask Application
 #
@@ -10,14 +10,19 @@
 
 import os
 
-MYSQL_HOST = os.environ.get('MYSQL_HOST', 'sakila-db-server')
-MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
-MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
-MYSQL_DB = os.environ.get('MYSQL_DB', 'sakila')
 
-try:
-    CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
-except ValueError:
-    CONNECTION_TIMEOUT = 30
-    
-HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
+class Config:
+    MYSQL_HOST = os.environ.get('MYSQL_HOST', 'sakila-db-server')
+    MYSQL_USER = os.environ.get('MYSQL_USER', 'root')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD', '')
+    MYSQL_DB = os.environ.get('MYSQL_DB', 'sakila')
+
+    try:
+        CONNECTION_TIMEOUT = int(os.environ.get('CONNECTION_TIMEOUT', '30'))
+    except ValueError:
+        CONNECTION_TIMEOUT = 30
+
+    try:
+        HEALTH_CHECK_INTERVAL = int(os.environ.get('HEALTH_CHECK_INTERVAL', '10'))
+    except ValueError:
+        HEALTH_CHECK_INTERVAL = 10
